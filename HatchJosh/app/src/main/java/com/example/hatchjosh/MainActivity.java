eedc36f63905b303c49058b5d580f56c24119d20
jhatc6948 <jhatc6948@westada.org>
2019-02-22 12:30:16 -0700

    finished calculator

--- HatchJosh/app/src/main/java/com/example/hatchjosh/MainActivity.java
+++ HatchJosh/app/src/main/java/com/example/hatchjosh/MainActivity.java
@@ -28,6 +28,55 @@ public class MainActivity extends AppCompatActivity {
                 resultTextView.setText(resultAdd + "");
 
             }
+
+        });
+        Button subBtn = (Button) findViewById(R.id.subBtn);
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                double num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                double resultAdd = num1 - num2;
+                resultTextView.setText(resultAdd + "");
+
+            }
+
+        });
+        Button multBtn = (Button) findViewById(R.id.multBtn);
+        multBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                double num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                double resultAdd = num1 * num2;
+                resultTextView.setText(resultAdd + "");
+
+            }
+
+        });
+        Button devBtn = (Button) findViewById(R.id.devBtn);
+        devBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                double num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                double resultAdd = num1 / num2;
+                resultTextView.setText(resultAdd + "");
+
+            }
+
         });
     }
 }
